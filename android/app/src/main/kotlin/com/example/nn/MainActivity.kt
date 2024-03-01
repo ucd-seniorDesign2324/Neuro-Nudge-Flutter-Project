262120ad835bb0c504556d0e4d5810df9fee1b13
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2024-03-01 11:03:03 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/nn/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.nn
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class MainActivity: FlutterActivity()
