7db9daf3bb90ecb8a34e6a5bfb49849b66f8db48
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2024-03-07 17:03:27 -0700

    Homepage structure created.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,88 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
+
+import 'package:nn/search_bar.dart';
 
     // TODO: 
     // Fetch event data and display on list tiles.
-    // Implement search bar. (Tutorials available on Youtube)
-    // Implement drawer menu. 
     // Define page navigations 
     // Define monthly task view (Separate into own file if you feel so lead)
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
   const HomePage({super.key, required this.items});
 
   final List<String> items;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final FloatingSearchBarController controller = FloatingSearchBarController();
+
+  @override
   Widget build(BuildContext context){
-    const String appTitle = 'Neuro Nudge';
-    return MaterialApp(
-      title: appTitle,
-      debugShowCheckedModeBanner: false,
-      // Basic theme. Will be modified down the road.
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        cardTheme: CardTheme(color: Colors.blue.shade50),
-        useMaterial3: true,
-      ),
+    const String appTitle = 'NeuroNudge';
+    return Scaffold(
+        appBar: appBar(appTitle),
 
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton( 
-            icon: const Icon( 
-              Icons.menu,
-              semanticLabel: 'menu',
-            ),
-            onPressed: () {},
-          ),
-          centerTitle: true,
-          title: const Text(appTitle),
+        drawer: Drawer( 
+          child: Column( 
+            children: [
+              const SizedBox(height: 75,),
+              const Divider(),
 
-          // TODO: Add search bar
+              // 
+              ListTile( 
+                title: const Center(
+                  child: Text(
+                    'Daily',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)
+                    )
+                  ),
+
+                onTap: (){
+                  Navigator.pop(context);
+                },
+              ),
+                
+              ListTile( 
+                title: const Center(
+                  child: Text(
+                    'Monthly',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)
+                    )
+                  ),
+                  onTap: (){
+                  Navigator.pop(context);
+                }
+              ),
+              
+              const Divider(),
+
+              ListTile( 
+                title: const Center(
+                  child: Text(
+                    'Home',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)
+                    )
+                  ),
+                  onTap: (){
+                  Navigator.pop(context);
+                }
+              ),
+
+              ListTile( 
+                title: const Center(
+                  child: Text(
+                    'Profile',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0)
+                    )
+                  ),
+                  onTap: (){
+                  Navigator.pop(context);
+                }
+              ),
+
+              const SizedBox(height: 230,),
+
+              Expanded(
+                child: Column(
+                  children: [
+                    Align( 
+                      alignment: FractionalOffset.bottomCenter,
+                      child: ListTile( 
+                        title: const Center(
+                          child: Text(
+                            'Logout',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0)
+                            )
+                          ),
+                          onTap: (){
+                            Navigator.pop(context);
+                          }
+                        ),
+                    ),
+
+                    Align( 
+                      alignment: FractionalOffset.bottomCenter,
+                      child: ListTile( 
+                        title: const Center(
+                          child: Text(
+                            'Help',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0)
+                            )
+                          ),
+                          onTap: (){
+                            Navigator.pop(context);
+                          }
+                        ),
+                      ),
+                  ],
+                )
+              )
+            ],
+          )
         ),
-
-        // TODO: Add drawer menu
         
         // TODO: Add floating action button
         
-        body: Center(
-          child: ListView.builder(
-            itemCount: items.length, 
+        body: Column(
+          children: [
+            Expanded(
+              child: listBuilder()
+            ),
+          ],
+        ),
+      );
+  }
 
-            // prototypeItem: ListTile(
-            //   title: Text(items.first),
-            // ),
+  Center listBuilder() {
+    // Generates cards
 
-            itemBuilder: (context, index){
-              return Card(
-                child: ListTile(
-                  leading: const CircleAvatar(child: Text("Sub")),
-                  title: Text("Event $index"),
-                  subtitle: Text("Task $index"),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.more_vert),
-                    onPressed: () { // Open task management view
-                      // TODO: Add navigation to task management view
-                    },),
-                )
+    return Center(
+        child: ListView.builder(
+          itemCount: widget.items.length, 
+
+          itemBuilder: (context, index){
+            return Card(
+              child: ListTile(
+                leading: const CircleAvatar(child: Text("Sub")),
+                title: Text("Event $index"),
+                subtitle: Text("Task $index"),
+                trailing: IconButton(
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () {
+                    // TODO: Add navigation to task management view
+                  },
+                ),
+              )
+            );
+          },
+        ),
+      );
+  }
+
+  AppBar appBar(String appTitle) {
+    return AppBar(
+
+        centerTitle: true,
+
+        title: Text( 
+          appTitle,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0)
+          ),
+
+        backgroundColor: const Color(0xffF5F5F5),
+
+        actions:[
+          IconButton( 
+            icon: const Icon( 
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            iconSize: 30.0,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SearchBarView()),
               );
             },
           ),
-        ),
-      )
-    );
+        ]
+      );
   }
 }
-
-
-// class SearchBarView extends StatelessWidget{
-//   const SearchBarView({super.key})
-// }
-
-
-// class DrawerMenuView extends StatelessWidget{
-//   const SideBarView({super.key})
-// }
 
 
