a279c6572f79137ea001788ab01d7fb0e590218a
Brian Hagerty <hagerty@sawatchlabs.com>
2024-03-08 10:53:40 -0700

    test commit

--- lib/task_management_view.dart
+++ lib/task_management_view.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 // Define class for adding task
 // Create methods for entering task data. I.e., task name, type, subtasks, etc
 // Implement callback methods for user interactions. I.e., onTouch()
+print("things");
