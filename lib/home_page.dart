ddfe090f7461f8a77a2000edce65c5dbfe0d209f
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2024-03-08 10:57:59 -0700

    Created settings class and drawer menu method.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
 import 'package:nn/search_bar.dart';
+import 'package:nn/methods/drawer_menu.dart';
 
     // TODO: 
     // Fetch event data and display on list tiles.
@@ -12,9 +13,9 @@ import 'package:nn/search_bar.dart';
 
 class HomePage extends StatefulWidget {
 
-  const HomePage({super.key, required this.items});
+  const HomePage({super.key});
 
-  final List<String> items;
+  // final List<String> items;
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -28,118 +29,10 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context){
     const String appTitle = 'NeuroNudge';
     return Scaffold(
+
         appBar: appBar(appTitle),
 
-        drawer: Drawer( 
-          child: Column( 
-            children: [
-              const SizedBox(height: 75,),
-              const Divider(),
-
-              // 
-              ListTile( 
-                title: const Center(
-                  child: Text(
-                    'Daily',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0)
-                    )
-                  ),
-
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-                
-              ListTile( 
-                title: const Center(
-                  child: Text(
-                    'Monthly',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0)
-                    )
-                  ),
-                  onTap: (){
-                  Navigator.pop(context);
-                }
-              ),
-              
-              const Divider(),
-
-              ListTile( 
-                title: const Center(
-                  child: Text(
-                    'Home',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0)
-                    )
-                  ),
-                  onTap: (){
-                  Navigator.pop(context);
-                }
-              ),
-
-              ListTile( 
-                title: const Center(
-                  child: Text(
-                    'Profile',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30.0)
-                    )
-                  ),
-                  onTap: (){
-                  Navigator.pop(context);
-                }
-              ),
-
-              const SizedBox(height: 230,),
-
-              Expanded(
-                child: Column(
-                  children: [
-                    Align( 
-                      alignment: FractionalOffset.bottomCenter,
-                      child: ListTile( 
-                        title: const Center(
-                          child: Text(
-                            'Logout',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30.0)
-                            )
-                          ),
-                          onTap: (){
-                            Navigator.pop(context);
-                          }
-                        ),
-                    ),
-
-                    Align( 
-                      alignment: FractionalOffset.bottomCenter,
-                      child: ListTile( 
-                        title: const Center(
-                          child: Text(
-                            'Help',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30.0)
-                            )
-                          ),
-                          onTap: (){
-                            Navigator.pop(context);
-                          }
-                        ),
-                      ),
-                  ],
-                )
-              )
-            ],
-          )
-        ),
+        drawer: drawerMenuBuilder(context),
         
         // TODO: Add floating action button
         
@@ -150,6 +43,16 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+        floatingActionButton: FloatingActionButton( 
+          backgroundColor: Colors.black,
+          onPressed: (){
+            Placeholder();
+          },
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 45,)
+        ),
       );
   }
 
@@ -158,7 +61,7 @@ class _HomePageState extends State<HomePage> {
 
     return Center(
         child: ListView.builder(
-          itemCount: widget.items.length, 
+          itemCount: 20, 
 
           itemBuilder: (context, index){
             return Card(
