f397040b8939ae51c4c6c864ae4cbb4dbc134814
Jake McKnight <jake.mcknight@ucdenver.edu>
2024-04-03 18:58:55 -0600

    supabase integrated, user account creation and login working on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:nn/presentation/home_page.dart';
-import 'package:nn/presentation/theme.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'data/firebase_options.dart';
+// import 'package:nn/presentation/home_page.dart';
+// import 'package:nn/presentation/theme.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'data/firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:nn/presentation/splash_page.dart';
+import 'package:nn/presentation/account_page.dart';
+import 'package:nn/presentation/login_page.dart';
 
 
 void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(options:
+  //   DefaultFirebaseOptions.currentPlatform,
+  // );
+
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: 
-    DefaultFirebaseOptions.currentPlatform,
+
+  await Supabase.initialize(
+    url: 'https://fgocfoakntmlhgtftrzh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnb2Nmb2FrbnRtbGhndGZ0cnpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE2ODkyMTUsImV4cCI6MjAyNzI2NTIxNX0.s5dAWy-DSa1EBfKjhpGOOcax6S7QUsh7xCHPFgKlBn8',
   );
 
   runApp(const ProviderScope(child: MyApp()));
-} 
+}
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     const String appTitle = 'Neuro Nudge';
     return MaterialApp(
-      title: appTitle,
-      theme: appThemeData,
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+            title: appTitle,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/account': (_) => const AccountPage(),
+      },
     );
   }
 }
